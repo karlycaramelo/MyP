3d49fc530a243a15fbfb2376884ed234c46119f7
jonathan <jobaji09@gmail.com>
2014-12-30 22:22:52 -0600

    algunos controladores de usuario

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
- 
-  get 'user/new'
+
+  get 'users/new'
 
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
-  get 'contact' =>  'static_pages#contact' 
- end
+  get 'contact' => 'static_pages#contact' 
+  get 'signup'  => 'user#new'
+  get 'welcome/index' 
+end
